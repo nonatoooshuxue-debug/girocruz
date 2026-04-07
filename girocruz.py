5293a161e7626b95915a9ec8f3f435a7ea37addf
Nonato <nonatoooshuxue@gmail.com>
2026-04-06 22:53:01 -0300

    Update girocruz.py

--- girocruz.py
+++ girocruz.py
@@ -14,18 +14,29 @@ def carregar():
         "https://www.googleapis.com/auth/drive"
     ]
     
-    info_dict = dict(st.secrets["controle"])
-    creds = service_account.Credentials.from_service_account_info(
-        info_dict, 
-        scopes=escopos
-    )
-    cred = pygsheets.authorize(service_account_credentials=creds)
+    info_dict = {
+        "type": st.secrets["controle"]["type"],
+        "project_id": st.secrets["controle"]["project_id"],
+        "private_key_id": st.secrets["controle"]["private_key_id"],
+        "private_key": st.secrets["controle"]["private_key"],
+        "client_email": st.secrets["controle"]["client_email"],
+        "client_id": st.secrets["controle"]["client_id"],
+        "auth_uri": st.secrets["controle"]["auth_uri"],
+        "token_uri": st.secrets["controle"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["controle"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["controle"]["client_x509_cert_url"],
+        "universe_domain": st.secrets.get("controle", {}).get("universe_domain", "googleapis.com")
+    }
+    
+    creds = service_account.Credentials.from_service_account_info(info_dict, scopes=escopos)
+    
+    client = pygsheets.client.Client(creds)
     
     url = "https://docs.google.com/spreadsheets/d/1ZDjG_6rmGTIMx2cs_0Fsf0V6T01AJhqrMeS2GczkXAw/edit"
-    arquivo = cred.open_by_url(url)
+    arquivo = client.open_by_url(url)
     aba = arquivo.worksheet_by_title("Base_Cruz")
-    df = aba.get_as_df()
-    return df
+    
+    return aba.get_as_df()
     
 df = carregar()
 df = df.loc[:, df.columns != ''] 
